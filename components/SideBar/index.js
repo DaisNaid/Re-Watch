36bac45035420e8b67cc671597ba7d1db6b15884
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-02 05:05:29 +0000

    Added Username Display

--- components/SideBar/index.js
+++ components/SideBar/index.js
@@ -1,34 +1,39 @@
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { UserContext } from '../../pages/_app';
 import icons from '../../utils/icons';
+
+//replace tryve with username from db -- line 18
 
 export default function SideBar() {
     
     const {setShowMenu} = useContext(UserContext);
 
   return (
-    <div className='fixed top-0 left-0 h-screen w-60 glass'>
-    <span className='absolute top-5 left-5 text-xl' onClick={() =>setShowMenu(false)}>
-        {icons.close}
-    </span>
-      <ul className='pt-32 mx-12'>
-        <li>
+    <div className='fixed top-0 left-0 h-screen w-48 glass'>
+      <span className='absolute top-5 left-5 text-xl' onClick={() =>setShowMenu(false)}>
+          {icons.close}
+      </span>
+      <div className='absolute bottom-5 left-5 text-purple-500'>{icons.verfied}
+        <span className='ml-2 text-white'>Tryve</span>
+      </div>
+      <ul className='pt-32 mx-4'>
+        <li onClick={() =>setShowMenu(false)}>
           <Link href='/' className='text-white flex items-center p-4'>
             <p className='px-4 comet'>Home</p>
           </Link>
         </li>
-        <li>
+        <li onClick={() =>setShowMenu(false)}>
           <Link href='/all' className='text-white flex items-center p-4'>
             <p className='px-4 comet'>All</p>
           </Link>
         </li>
-        <li>
+        <li onClick={() =>setShowMenu(false)}>
           <Link href='/music' className='text-white flex items-center p-4'>
             <p className='px-4 comet'>Music Libray</p>
           </Link>
         </li>
-        <li>
+        <li onClick={() =>setShowMenu(false)}>
           <Link href='/wallpaper' className='text-white flex items-center p-4'>
             <p className='px-4 comet'>Wallpapers</p>
           </Link>
--- utils/icons/index.js
+++ utils/icons/index.js
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faBars, faXmark} from '@fortawesome/free-solid-svg-icons';
+import {faBars, faXmark, faCertificate} from '@fortawesome/free-solid-svg-icons';
 
 const icons = {
     menu: <FontAwesomeIcon icon={faBars} />,
-    close: <FontAwesomeIcon icon={faXmark} />
+    close: <FontAwesomeIcon icon={faXmark} />,
+    verfied: <FontAwesomeIcon icon={faCertificate} />,
 };
 
 export default icons;
