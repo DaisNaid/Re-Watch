46d7afa3864bf48fd9c510d3c5e07309bb8f443a
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-21 19:41:01 +0000

    Reimplemented password authentication

--- components/LoginModal/index.js
+++ components/LoginModal/index.js
@@ -1,11 +1,11 @@
 import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../../pages/_app";
-// import icons from "../../utils/icons";
-// import Form from 'react-bootstrap/Form';
+import icons from "../../utils/icons";
+import Form from 'react-bootstrap/Form';
 
 export default function LoginModal({appID}) {
-    // const { setIsUser } = useContext(UserContext);
-    // const [username, setUsername] = useState('');
+    const { setIsUser } = useContext(UserContext);
+    const [username, setUsername] = useState('');
 
     useEffect(() => {
       require('@passageidentity/passage-auth');
@@ -15,17 +15,23 @@ export default function LoginModal({appID}) {
     <div className="absolute opacity-100 z-10 pointer-events-auto
     left-0 right-0 top-0 bottom-0 block
     transition-all duration-300 ease-in-out delay-0 glass">
-      <div className="absolute top-40 left-[3.5rem] bg-zinc">
-        <passage-auth app-id={appID} ></passage-auth>
-      </div>
+      <Form className='absolute bg-zinc top-28 left-8 mx-20 pt-8 p-4 text-center text-darkanime flex flex-col gap-4'>
+      <span className="absolute top-3 left-3" onClick={() => setIsUser(false)}>{icons.close}</span>
+        <div className='flex flex-col gap-4 mt-4'>
+        <Form.Group controlId="username">
+            <Form.Control
+              type="text"
+              placeholder="Enter Username"
+              className="text-center bg-white text-black"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </Form.Group>
+        </div>
+        <button disabled={username === ''} className="text-green-600">{icons.signIn}</button>
+        <div className='text-sm'>Continue with Google</div>
+        <span className='text-sm'>No account? Sign Up</span>
+      </Form>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  return {
-    props: {
-      appID: process.env.PASSAGE_APP_ID
-    }
-  }
 }
