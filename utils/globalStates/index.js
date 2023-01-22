d84710910ab23c5aab616970c44cad27b337e6dd
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-22 19:39:30 +0000

    Stopping reload from resetting userMode preference

--- utils/globalStates/index.js
+++ utils/globalStates/index.js
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function useGlobalState() {
+    const [defaultMode, setDefaultMode] = useState(false);
+    
+    useMediaQuery(
+        {
+          query: '(prefers-color-scheme: dark)',
+        },
+        undefined,
+        (isSystemDark) => setDefaultMode(isSystemDark)
+    );
+        
     const [showMenu, setShowMenu] = useState(false);
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(defaultMode);
     const [selectedTab, setSelectedTab] = useState('All');
     const [isUser, setIsUser] = useState(false);
     const [dominantColor, setDominantColor] = useState('#d4d4d8');
