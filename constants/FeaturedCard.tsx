b73ce621e18f554367f2c1c7055f5f37d5b6d066
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-22 12:10:07 +0000

    Removed button opacity

--- constants/FeaturedCard.tsx
+++ constants/FeaturedCard.tsx
@@ -5,10 +5,10 @@ const featuredCard:ClassName = {
     containerDark:'my-4 px-4 absolute left-[4rem] mt-16',
     imageLight:'border-2 border-lightanime rounded-[0.95rem]',
     imageDark:'border-2 border-dark_inner rounded-[0.95rem]',
-    previewLight:'px-2.5 flex flex-row gap-1 absolute top-[82%] left-6 bg-white text-black rounded-md cursor-pointer opacity-80',
-    previewDark:'px-2.5 flex flex-row gap-1 absolute top-[82%] left-6 bg-white text-black rounded-md cursor-pointer opacity-80',
-    myListLight:'px-2.5 flex flex-row gap-1 absolute top-[82%] right-6 bg-light_inner rounded-md cursor-pointer opacity-80',
-    myListDark:'px-2.5 flex flex-row gap-1 absolute top-[82%] right-6 bg-dark_inner rounded-md cursor-pointer opacity-80'
+    previewLight:'px-2.5 flex flex-row gap-1 absolute top-[82%] left-6 bg-white text-black rounded-md cursor-pointer ',
+    previewDark:'px-2.5 flex flex-row gap-1 absolute top-[82%] left-6 bg-white text-black rounded-md cursor-pointer ',
+    myListLight:'px-2.5 flex flex-row gap-1 absolute top-[82%] right-6 bg-light_inner rounded-md cursor-pointer ',
+    myListDark:'px-2.5 flex flex-row gap-1 absolute top-[82%] right-6 bg-dark_inner rounded-md cursor-pointer '
 };
 
 export default featuredCard;
