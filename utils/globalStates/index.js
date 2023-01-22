8270dc2ac1b59a6ddfbe05dfe9de1d126af5e121
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-22 22:01:25 +0000

    Localstorage #6

--- utils/globalStates/index.js
+++ utils/globalStates/index.js
@@ -2,16 +2,15 @@ import { useEffect, useRef, useState } from "react";
 
 export default function useGlobalState() {
     const defaultMode = useRef(false);
-
-    useEffect(() => {
-        defaultMode.current = localStorage.getItem('darkmode')
-    }, [isDarkMode])
-        
     const [showMenu, setShowMenu] = useState(false);
     const [isDarkMode, setIsDarkMode] = useState(defaultMode.current);
     const [selectedTab, setSelectedTab] = useState('All');
     const [isUser, setIsUser] = useState(false);
     const [dominantColor, setDominantColor] = useState('#d4d4d8');
+
+    useEffect(() => {
+        defaultMode.current = localStorage.getItem('darkmode')
+    }, [isDarkMode])
 
     const globalStates = {
         showMenu,
