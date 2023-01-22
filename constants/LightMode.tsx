5ef8a41beaedfe40356cbcc1379e8eb967cc270c
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-22 01:45:27 +0000

    CSS Test #6

--- constants/LightMode.tsx
+++ constants/LightMode.tsx
@@ -2,7 +2,7 @@ import { ClassName } from "../utils/types/constantTypes";
 
 const light:ClassName = {
     main_bg: 'bg-zinc',
-    motto: 'text-center bg-light_inner text-black animate-glow_motto absolute top-[60vh] left-[22vw] p-4 border-2 border-lightanime'
+    motto: 'text-center bg-light_inner text-black animate-glow_motto absolute top-[75vh] left-[22vw] p-4 border-2 border-lightanime'
 };
 
 export default light;
