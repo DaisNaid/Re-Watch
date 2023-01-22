b121f8142151b60d1be0827f6c90d1c0ec0734fd
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-22 11:01:42 +0000

    Closed up gap between buttons

--- components/FeaturedCard/index.js
+++ components/FeaturedCard/index.js
@@ -8,7 +8,7 @@ export default function FeaturedCard ({containerStyle, imageStyle, previewStyle,
         <div className={imageStyle}>
             <Image alt="Alchemy of Souls" src={series[0].image} className='rounded-xl h-[23.5rem] w-60' />
         </div>
-        <div className='flex flex-row gap-4 py-2'>
+        <div className='flex flex-row gap-2 py-2'>
             <div className={previewStyle}>
                 <span className='py-2 pl-2'>{icons.play}</span>
                 <span className='py-2 pr-2'>Preview</span>
