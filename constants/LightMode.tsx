3a19a27e37ce9b1c9bd6ec4cbd99638907426de5
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-01-23 22:14:55 +0000

    Little change to parallax

--- constants/LightMode.tsx
+++ constants/LightMode.tsx
@@ -3,7 +3,7 @@ import { ClassName } from "../utils/types/constantTypes";
 const light:ClassName = {
     main_bg: 'dominantColorBG',
     motto: 'text-center bg-light_inner text-black animate-glow_motto p-4 border-y-2 border-y-lightanime',
-    fixed_motto: 'fixed top-0 min-w-full text-center bg-light_inner text-black animate-glow_motto p-4 border-y-2 border-y-lightanime'
+    fixed_motto: 'fixed top-0 min-w-full text-center bg-light_inner text-black animate-glow_motto p-4 border-b-2 border-b-lightanime'
 };
 
 export default light;
