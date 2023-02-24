13cce7bf45fb1484a046b9c3b9de3aca4be3a724
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-02-24 20:22:43 +0000

    Testing image css change

--- constants/SeriesPage.tsx
+++ constants/SeriesPage.tsx
@@ -2,7 +2,7 @@ import { ClassName } from "../utils/types/constantTypes";
 
 const seriesPage:ClassName = {
     content: 'mx-4',
-    bgImage: 'w-fit shadow-2xl',
+    bgImage: 'w-full shadow-2xl',
     image: `max-w-[40vw] relative top-[-4rem]
         rounded-md shadow-sm shadow-red-500 mix-blend-normal backdrop-blur-sm`,
     circle: 'bg-transparent h-6 w-6 rounded-circle border-2 border-marvelLight',
